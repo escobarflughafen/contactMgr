3adae73ffa3ddb4241bbf93d411303643433542c
bi11gates <yumekeeper@gmail.com>
2018-04-23 20:45:39 +0800

    added UI componets of Contact sheet

--- src/main.java
+++ src/main.java
@@ -4,10 +4,15 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import ui.contacts;
 
 public class main {
 
     public static void main(String[] args){
-
+        new contacts();
+        contacts.main();
     }
 }
---
+++ src/utils/contactUtils.java
@@ -0,0 +1,8 @@
+package utils;
+
+import classes.member;
+
+
+public class contactUtils {
+
+}
